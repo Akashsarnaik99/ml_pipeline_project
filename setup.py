f36c99248d7085240f975c1a97e310a002bdd8c6
Akashsarnaik <akash.sarnaik99@gmail.com>
2024-04-29 12:39:47 +0530

    basic structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(filepath:str) -> List[str]:
+    requirements =[]
+
+
+    with open(filepath) as file_obj:
+        requirements=file_obj.readlines()
+        requirements = [ i.replace('\n','') for  i in requirements]
+
+        if HYPHEN_E_DOT  in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+
+
+setup(name='ML_Pipeline_project',
+      version='0.0.1',
+      description='Machine Learning Pipeline Project',
+      author='Akash Sarnaik',
+      author_email='akashsarnaik99@gmail.com',
+      packages=find_packages(),
+      install_requires = get_requirements("requirements.txt")
+     )
